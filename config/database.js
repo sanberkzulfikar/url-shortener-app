e72bfa1f632893a0cb426d722a07a0851966ce7b
sanberkzulfikar <tosanberk@icloud.com>
2023-02-23 04:31:11 +0300

    .

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.DB_STRING, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true
         })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
